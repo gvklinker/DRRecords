428b5f036d0a3044db731cdfb8e3738401ef0f0e
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-11-13 11:47:53 +0100

    YAML

--- DRRecords/Domain/Record.cs
+++ DRRecords/Domain/Record.cs
@@ -2,6 +2,24 @@
 {
     public class Record
     {
+        public int Id { get; set; }
 
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public void Validate()
+        {
+            {
+                if (Title == null)
+                    throw new ArgumentNullException(nameof(Title), "Title is null");
+                if (Title.Length < 1)
+                    throw new ArgumentException("Title must be at least 2 characters: " + Title);
+                if (Artist.Length < 1)
+                    throw new ArgumentOutOfRangeException(nameof(Artist), "Artist must be at least 2 characters: " + Artist);
+                if(Artist == null)
+                    throw new ArgumentNullException(nameof(Artist), "Artist is null");
+            }
+        }
     }
 }
---
+++ DRRecordsTests/Domain/RecordTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DRRecords.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRRecords.Domain.Tests
+{
+    [TestClass()]
+    public class RecordTests
+    {
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
